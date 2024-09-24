acadb61723d382a6edf893b6b514e1729578e3dd
antoniolago <45375617+antoniolago@users.noreply.github.com>
2024-09-23 21:08:41 -0300

    add missing packages

--- internal/controller/clusterlimitrange_controller.go
+++ internal/controller/clusterlimitrange_controller.go
@@ -26,6 +26,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+    "sigs.k8s.io/controller-runtime/pkg/handler"
+    "sigs.k8s.io/controller-runtime/pkg/source"
 	lag0v1 "lag0.com.br/cluster-limit-range-controller/api/v1"
 )
 
